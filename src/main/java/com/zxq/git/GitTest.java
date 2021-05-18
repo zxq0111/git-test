00aec1f07cbefc808328c9a48dd9f0889e866afb
zxq <758687769@qq.com>
2021-05-18 16:16:55 +0800

    first commit

---
+++ src/main/java/com/zxq/git/GitTest.java
@@ -0,0 +1,14 @@
+package com.zxq.git;
+
+/**
+ * @Author: zxq
+ * @Date: 2021/5/18
+ * @Time: 16:09
+ * @Description:
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
