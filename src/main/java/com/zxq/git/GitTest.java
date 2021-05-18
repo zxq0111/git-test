7d740ed021966a2773d0c9193769b683c329b149
zxq <758687769@qq.com>
2021-05-18 17:08:21 +0800

    hot-fix commit1

--- src/main/java/com/zxq/git/GitTest.java
+++ src/main/java/com/zxq/git/GitTest.java
@@ -12,6 +12,7 @@ public class GitTest {
         System.out.println("hello git!");
         System.out.println("hello git2!");
         System.out.println("hello git3!");
+        System.out.println("hello git4!");
 
     }
 }
